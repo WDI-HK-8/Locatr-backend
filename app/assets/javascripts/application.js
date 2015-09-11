10d844d0d8d8a38ea6ff353cc3c085e0700ef346
raptattention <klaw1794@gmail.com>
2015-09-11 09:55:44 +0800

    application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery/dist/jquery
+//= require jquery/dist/jquery.min.js
 //= require_tree .
